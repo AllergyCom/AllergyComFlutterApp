f9ec0351ada3d32a5247d97195293b2a74617328
Kesevan <kesevanbalaji@gmail.com>
2023-05-13 20:32:48 +0530

    INFURA OP

--- lib/constant/Constant.dart
+++ lib/constant/Constant.dart
@@ -1,3 +1,3 @@
-String infura_url = "https://sepolia.infura.io/v3/3fabd95312fb497aaa36316e7dfa1f9a";
+String infura_url = "https://sepolia.infura.io/v3/5e5e38fc8abd46cc91b0e0bdd2b22626";
 String infuraAllergyCom_privateKey = "7379041e943d0d039d8b7d7013cdcf20b8780d3b85f9ccaa7fdb17952ead688f";
-String contractAdress1 = "0x17fBEaD3eF46af033771B4DA1E321da896Ded5fa";
+String contractAdress1 = "0xd1dC984975A464d3874deD1a61553bb1f35A5E57";
